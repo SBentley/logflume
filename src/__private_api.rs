0efa26164da68f7d4f173fcbc2f63c39fe09bfdd
Sam Bentley <samtbentley@gmail.com>
2024-04-12 09:52:07 +0100

    add line numbers

--- src/__private_api.rs
+++ src/__private_api.rs
@@ -2,6 +2,6 @@ use crate::Level;
 pub use crate::LoggingFunc;
 pub use std::format_args;
 
-pub fn log(level: Level, func: crate::LoggingFunc, file: &'static str) {
-    crate::logger().log(level, func, file)
+pub fn log(level: Level, func: crate::LoggingFunc, file: &'static str, line: u32) {
+    crate::logger().log(level, func, file, line)
 }
